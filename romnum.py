805d4a0cf9fa999ffe9c395d77fc217b4116a227
igbenc <72074450+igbenc@users.noreply.github.com>
2022-03-16 11:23:34 +0100

    Add files via upload

---
+++ romnum.py
@@ -0,0 +1,24 @@
+def num():
+    number = int(input("enter a number"))
+    iterator = 0
+    num = ""
+    factual_numbers = {
+        1 : "I",
+        4 : "IV",
+        5 : "V",
+        9 : "IX",
+        10 : "X",
+        40 : "XL",
+        50 : "L",
+        90 : "XC",
+        100 : "C",
+        400 : "CD",
+        500 : "D",
+        900 : "CM",
+        1000 : "M"
+    }
+    while number != 0:
+        for number in (factual_numbers.keys():
+            if number < 5:
+                print (number)
+num()
